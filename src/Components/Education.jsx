b7f39221dbd706175f943f278bf8fc2ed782da28
Nataly Alejandra Gemio Morales <144859461+TuGfaNatMLSA@users.noreply.github.com>
2023-10-05 21:49:48 +0000

    ya estaria todo menos lo de esas animaciones pero esta implementado

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,34 +1,8 @@
-/**
- * Portfolio component
- *
- * Highlights some of  your creations. These can be designs, websites,
- * open source contributions, articles you've written and more.
- *
- * This is a great area for you to to continually add to and refine
- * as you continue to learn and create.
- */
-
 import React from "react";
 
-/**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
-import image from "../images/foto2.jpg";
+import image from "../images/cer.jpg";
+const imageAltText = "Woman with certification";
 
-const imageAltText = "Woman between plants";
-
-/**
- * Project list
- *
- * An array of objects that will be used to display for your project
- * links section. Below is a sample, update to reflect links you'd like to highlight.
- */
 const projectList = [
   {
     title: "AI Fundamentals",
@@ -85,10 +59,10 @@ const projectList = [
   },
 ];
 
-const Portfolio = () => {
+const Education = () => {
   return (
-    <section className="padding" id="portfolio">
-      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+    <section className="padding" id="Education">
+      <h2 style={{ textAlign: "center" }}>Education</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
@@ -117,4 +91,4 @@ const Portfolio = () => {
   );
 };
 
-export default Portfolio;
+export default Education;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,15 +5,15 @@
  * choice, name and title that describes your career focus.
  */
 
+import PropTypes from "prop-types";
 import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
-import PropTypes from "prop-types";
 
 /**
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  *
@@ -27,13 +27,17 @@ const imageAltText = "Adult female in an event of Conecta Mentora";
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src={image} alt="" />
+      <img className="background" src={image} alt={imageAltText} />
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        <img
+          src={arrowSvg}
+          style={{ height: "3rem", width: "3rem", animation: "backwards infinite 1s ease-in-out" }}
+          alt={imageAltText}
+        />
       </div>
     </section>
   );
